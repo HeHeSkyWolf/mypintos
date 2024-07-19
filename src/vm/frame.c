6c051b7aae939beade8328418ee7c61fe120fa73
Brian Lai <67125129+HeHeSkyWolf@users.noreply.github.com>
2024-07-18 23:05:12 -0400

    global frame table

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -4,6 +4,20 @@
 #include "threads/thread.h"
 #include "vm/frame.h"
 
+static struct list frame_table;
+
+void
+frame_table_init (void)
+{
+  list_init (&frame_table);
+}
+
+void
+add_frame_to_table (struct frame_data *frame)
+{
+  list_push_back (&frame_table, &frame->elem);
+}
+
 struct frame_data *
 frame_init (struct thread *t, void *vaddr)
 {
--- src/vm/frame.h
+++ src/vm/frame.h
@@ -7,4 +7,6 @@ struct frame_data {
   size_t pfn; /* Physical Frame Number */
 };
 
+void frame_table_init (void);
+void add_frame_to_table (struct frame_data *frame);
 struct frame_data *frame_init (struct thread *t, void *vaddr);
